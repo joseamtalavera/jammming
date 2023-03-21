6dba9fe0b8644d48f62c74b665bcd93f028d8886
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-03-21 19:59:49 +0200

    components-1

---
+++ src/Components/App/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {SearchBar} from './SearchBar/'
+import {SearchResults} from './SearchResults'
+import {Playlist} from './Playlist'
+
+function App() {
+  return (
+ <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar />
+    <div class="App-playlist">
+      <SearchResults />
+      <Playlist />
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
